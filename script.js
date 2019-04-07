a1adf4edb3cdb50ff8fc49fbc967b14dd69cf458
Adem-Ch <adem-ch@ya.ru>
2019-04-08 00:13:40 +0300

    new pages

---
+++ script.js
@@ -0,0 +1,10 @@
+// alert( "Script from file!!!" );
+// alert(admin + "_wefSDF");
+//
+// for (let i = 0; i < 2; i++) {
+//     alert("_For_1-" + i);
+// }
+
+
+var userName = prompt("Enter your name", '');
+document.getElementById("userName").innerHTML = userName;
